6d295323094a04bb73d7b795ad9d8ece94efbe16
sydykovislam <sydykovislam03@gmail.com>
2021-08-30 17:04:36 +0600

    initializing repository

---
+++ asdvasd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  asdvasd
+//
+//  Created by Sydykov Islam on 8/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ asdvasd/asdvasdApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdvasdApp.swift
+//  asdvasd
+//
+//  Created by Sydykov Islam on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct asdvasdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
